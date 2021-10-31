63fe98d6efd82e6d184f9299799cc88b16285acc
lilXA1 <72554214+lilXA1@users.noreply.github.com>
2021-10-31 23:05:25 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-chjvhm_sO_xQMVM4HfVGAqcN1N4Iswo",
-    authDomain: "linkedin-clone-74b73.firebaseapp.com",
-    projectId: "linkedin-clone-74b73",
-    storageBucket: "linkedin-clone-74b73.appspot.com",
-    messagingSenderId: "305549450359",
-    appId: "1:305549450359:web:f7ef1644c856687af77a72",
+    apiKey: "o",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(
